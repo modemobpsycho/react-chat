50b87f18be683cdd40d6268c8cd2ba7bb3bfd028
Vadim Taratuta <hawaiianwork@mail.ru>
2024-06-22 20:41:15 +0300

    del useless file and refactor

--- frontend-chat/src/App.tsx
+++ frontend-chat/src/App.tsx
@@ -1,11 +1,51 @@
+import { Header } from './components/shared/Header'
 import './styles/App.scss'
-import Counter from './components/Counter'
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { store } from './store/store'
+import { Provider } from 'react-redux'
+import AuthPage from './views/AuthPage'
+import { ChatsPage } from './views/ChatsPage'
+import LoginForm from './components/LoginForm'
+import RegisterForm from './components/RegisterForm'
 
+const Layout = () => (
+	<>
+		<Header />
+		<Outlet />
+	</>
+)
 function App() {
+	const router = createBrowserRouter([
+		{
+			element: <Layout />,
+			children: [
+				{
+					path: '/login',
+					element: (
+						<AuthPage>
+							<LoginForm />
+						</AuthPage>
+					),
+				},
+				{
+					path: '/sign-up',
+					element: (
+						<AuthPage>
+							<RegisterForm />
+						</AuthPage>
+					),
+				},
+				{
+					path: '/chats',
+					element: <ChatsPage />,
+				},
+			],
+		},
+	])
 	return (
-		<>
-			<Counter />
-		</>
+		<Provider store={store}>
+			<RouterProvider router={router} />
+		</Provider>
 	)
 }
 
